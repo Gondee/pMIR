bd97699382955e67d1265b0b5b51f78c7df8dae5
Miguel Clement <miguelfclement@gmail.com>
2016-03-07 18:35:12 -0600

    added libraries and ble plugins

---
+++ www/scripts/NIR_API.js
@@ -0,0 +1,50 @@
+﻿angular.module('starter.services', [])
+
+.factory('BLE', function () {
+    var connected;
+
+    function btFailure(stage, error) { console.log("Bluetooth error during " + stage + " stage. Error: " + error); };
+
+    return {
+        devices: [],
+
+        scan: function (callback) {
+            var that = this;
+
+            ble.scan([], 5, function (device) {
+                console.log(JSON.stringify(device));
+            }, function (error) {
+                console.log("ERROR: " + console.log(error));
+                callback();
+            });
+
+
+        },
+        connect: function (deviceId, callback) {
+            if (deviceId == 0)
+                deviceId = "30:14:06:24:12:98";
+            /*bluetoothSerial.connect(deviceId,
+
+                function () {
+                   //success
+                    console.log("bluetooth connected!");
+                    callback(true);
+                },
+                callback(false)
+            );*/
+        },
+        disconnect: function (callback) {
+            /*bluetoothSerial.disconnect(
+
+                function () {
+                    //success
+                    console.log("bluetooth disconnected!");
+                    callback(false);
+                }, callback(true)
+            );*/
+        },
+        write: function (value) {
+            //bluetoothSerial.write(value, function () { console.log("SUCCESS = " + value) }, function () { console.log("FAIL= " + value) });
+        }
+    };
+});
---
+++ www/scripts/controllers.js
@@ -0,0 +1,25 @@
+﻿angular.module('control', [])
+
+.controller('DashCtrl', function ($scope, BLE) {
+
+    /*$scope.btConnect = function (deviceId) {
+        $scope.spinner = true;
+        BT.connect(
+            deviceId,
+            function (val) {
+                console.log("con val = " + val);
+                $scope.connected = val;
+                $scope.spinner = !val;
+                if (!$scope.$$phase) { $scope.$apply() }
+            }
+        );
+    };
+    $scope.btDisconnect = function (deviceId) {
+        $scope.spinner = true;
+        BT.disconnect(function (val) { console.log("disc val = " + val); $scope.connected = val; $scope.spinner = val; if (!val) { $scope.$apply() } });
+    };
+
+    $scope.btWrite = function (value) {
+        BT.write(value);
+    };*/
+})
