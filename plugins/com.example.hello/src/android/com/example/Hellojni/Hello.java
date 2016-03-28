50b9bd82f3aaa8d380657bc2eb13491de27a3f92
Miguel Clement <miguelfclement@gmail.com>
2016-03-27 19:07:36 -0500

    latest NDK and Plugin

---
+++ plugins/com.example.hello/src/android/com/example/Hellojni/Hello.java
@@ -0,0 +1,28 @@
+package com.example.plugin;
+
+import org.apache.cordova.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Hello extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+
+        if (action.equals("greet")) {
+
+            String name = data.getString(0);
+            String message = "Hello, " + name;
+            callbackContext.success(message);
+
+            return true;
+
+        } else {
+            
+            return false;
+
+        }
+    }
+}
